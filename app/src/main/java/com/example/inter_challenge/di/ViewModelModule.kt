05584333662fc29a10353c3a3e26e903b4a3c0b1
Miguel Yengle <miguelyeac@hotmail.com>
2022-08-24 09:49:29 -0300

    adjust viewmodel module

--- app/src/main/java/com/example/inter_challenge/di/ViewModelModule.kt
+++ app/src/main/java/com/example/inter_challenge/di/ViewModelModule.kt
@@ -1,9 +1,14 @@
 package com.example.inter_challenge.di
 
+import com.example.inter_challenge.viewmodel.PullRequestViewModel
+import com.example.inter_challenge.viewmodel.SearchListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
-    single {  }
+    viewModel { SearchListViewModel(get()) }
+
+    viewModel { PullRequestViewModel(get()) }
 
 }
