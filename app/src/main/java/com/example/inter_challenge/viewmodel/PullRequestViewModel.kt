e3dac2c45efa6150a93d616cf9bdd1f599785137
Miguel Yengle <miguelyeac@hotmail.com>
2022-08-24 10:39:26 -0300

    added view model files

---
+++ app/src/main/java/com/example/inter_challenge/viewmodel/PullRequestViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.inter_challenge.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.inter_challenge.repository.PullRequestRepository
+
+class PullRequestViewModel(
+    private val repository: PullRequestRepository
+) : ViewModel() {
+
+    //viewstate
+
+}
---
+++ app/src/main/java/com/example/inter_challenge/viewmodel/SearchListViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.inter_challenge.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.inter_challenge.repository.SearchRepository
+
+class SearchListViewModel(
+    private val repository: SearchRepository
+) : ViewModel() {
+
+    //viewstate
+
+}
